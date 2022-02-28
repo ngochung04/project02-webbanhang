d76a15a56acd3db7887e0e0040a62d083eb149e9
ngochung04 <dangngochung0401@gmail.com>
2022-02-28 17:01:48 +0700

    fix: netlify build error

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-light text-center text-lg-start">
       <div className="text-center p-3">
         © 2020 Copyright:
-        <a href="#">WebBanHang</a>
+        <a href="#/">WebBanHang</a>
       </div>
     </footer>
   );
